1f51f432d430c525febd839ef90fd8a5c739caf1
Lucas <lucas.jenvrain@gmail.com>
2021-11-16 17:14:57 +0100

    Getter Setter id et nom prenom et constructeur

--- src/petanquec3/Joueur.java
+++ src/petanquec3/Joueur.java
@@ -11,4 +11,40 @@ package petanquec3;
  */
 public class Joueur {
     
+      private String nom,prenom;
+      private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+      
+     
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    
+    public Joueur(){}
+
+    public Joueur(int id ,String nom, String prenom) {
+        this.id = id;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
 }
