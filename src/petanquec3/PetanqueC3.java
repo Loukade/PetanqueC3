87b4cd1cd2b6dd2b9e950f99b7cc498537c19e31
Lucas <lucas.jenvrain@gmail.com>
2021-11-17 09:34:25 +0100

    Ajout

--- src/petanquec3/PetanqueC3.java
+++ src/petanquec3/PetanqueC3.java
@@ -4,19 +4,45 @@
  * and open the template in the editor.
  */
 package petanquec3;
+import java.util.Random;
 
-/**
- *
- * @author lucas
- */
 public class PetanqueC3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //
-    }
-    
+        int NbEquipe ;
+        NbEquipe = (int)(Math.random()*14);
+        if(NbEquipe % 2 == 1)
+        {
+            NbEquipe = 0;
+        while(NbEquipe < 6)
+        {
+            NbEquipe = (int)(Math.random()*14);
+            if(NbEquipe % 2 == 1)
+            {
+            System.out.println(NbEquipe);
+            NbEquipe = 0;
+            }
+        }
+        }
+        else 
+        {
+         while(NbEquipe < 6)
+         {
+            NbEquipe = (int)(Math.random()*14);
+            if(NbEquipe % 2 == 1)
+            {
+            System.out.println(NbEquipe);
+            NbEquipe = 0;
+            }   
+         }
+  
+        }
+ System.out.println(NbEquipe);
+ int i;
+ for(i=0 ; i<NbEquipe;i++)
+ {
+     Equipe d = new Equipe(i,"Equipe"+(i+1));
+     System.out.println(d.getNom());
+ }
+}
 }
