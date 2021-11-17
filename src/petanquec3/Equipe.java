d216d36cd0374266d804c1f61e2df57f5fd95f6c
Lucas <lucas.jenvrain@gmail.com>
2021-11-17 09:34:40 +0100

    Ajout

--- src/petanquec3/Equipe.java
+++ src/petanquec3/Equipe.java
@@ -10,5 +10,28 @@ package petanquec3;
  * @author lucas
  */
 public class Equipe {
+    private int id ; 
+    private String nom ;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Equipe(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
     
 }
